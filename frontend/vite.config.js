b78d97797c4664ce05f4c297ad32125657cf070a
KaranKhedkar <karank200410@gmail.com>
2026-08-25 01:43:58 +0530

    fix: change python True to js true in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
-        changeOrigin: True,
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
